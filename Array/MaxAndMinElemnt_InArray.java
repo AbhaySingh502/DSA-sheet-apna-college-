4a0029453312731b5c864b93557f96cc143c84c4
AbhaySingh502 <abhaysingh65534@gmail.com>
2023-03-02 17:47:01 +0530

    Max and Min Element in an array

---
+++ Array/MaxAndMinElemnt_InArray.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.*;
+
+public class MaxAndMinElemnt_InArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println(max + " " + min);
+    }
+}
