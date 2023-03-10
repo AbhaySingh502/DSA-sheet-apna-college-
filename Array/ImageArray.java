11057ce0d262bc6cad76ce9b1aeac9bea5bec7c7
AbhaySingh502 <abhaysingh65534@gmail.com>
2023-03-10 09:51:53 +0530

    rotate image

---
+++ Array/ImageArray.java
@@ -0,0 +1,37 @@
+package Array;
+
+import java.util.*;
+
+public class ImageArray {
+
+    public static void rotate(int[][] arr) {
+        int m[][] = new int[arr.length][arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                m[j][arr.length - i - 1] = arr[i][j];
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(m[i][j]);
+            }
+            // System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Size of array: ");
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        System.out.println("Elements in your array: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        rotate(arr);
+    }
+}
--- Array/kthmissing.java
+++ Array/kthmissing.java
@@ -4,18 +4,35 @@ import java.util.*;
 
 public class kthmissing {
     public static void printKth(int ar[], int k) {
-        ArrayList<Integer> ls = new ArrayList<>();
-        for (int i = 0; i < ar.length; i++) {
-            for (int j = 1;; j++) {
-                if (ar[i] != j) {
-                    ls.add(j);
-                }
-                if (ls.size() == k) {
-                    ls.get(k);
-                    break;
-                }
+        // ArrayList<Integer> ls = new ArrayList<>();
+        // for (int i = 0; i < ar.length; i++) {
+        // for (int j = 1;; j++) {
+        // if (ar[i] != j) {
+        // ls.add(j);
+        // }
+        // if (ls.size() == k) {
+        // ls.get(k);
+        // break;
+        // }
+        // }
+        // }
+    }
+
+    public static int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int missingCount = 0;
+        int lastSeen = 0;
+
+        for (int i = 0; i < n; i++) {
+            int current = arr[i];
+            missingCount += current - lastSeen - 1;
+            if (missingCount >= k) {
+                return lastSeen + k - (missingCount - (current - lastSeen - 1));
             }
+            lastSeen = current;
         }
+
+        return lastSeen + k - missingCount;
     }
 
     public static void main(String args[]) {
@@ -29,6 +46,6 @@ public class kthmissing {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        printKth(arr, k);
+        findKthPositive(arr, k);
     }
 }
